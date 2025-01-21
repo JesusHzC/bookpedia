67a76bc712fa957f26da1f143fc49361ec44876d
jesusclemente <clementej@globalhitss.com>
2025-01-21 17:54:37 -0600

    Ui for BookListScreen completed

--- composeApp/src/androidMain/kotlin/com/jesushz/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/jesushz/bookpedia/Previews.kt
@@ -5,6 +5,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jesushz.bookpedia.book.domain.Book
+import com.jesushz.bookpedia.book.presentation.book_list.BookListScreen
+import com.jesushz.bookpedia.book.presentation.book_list.BookListState
 import com.jesushz.bookpedia.book.presentation.book_list.components.BookSearchBar
 
 @Preview(showBackground = true)
@@ -18,4 +21,30 @@ fun BookSearchBarPreview() {
             modifier = Modifier.fillMaxWidth()
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BookListScreenPrev() {
+    val books = (1..100).map {
+        Book(
+            id = it.toString(),
+            title = "Book $it",
+            authors = listOf("Author $it"),
+            imageUrl = "https://picsum.photos/200/300?random=$it",
+            averageRating = it.toDouble(),
+            description = "Description $it",
+            languages = listOf("Language $it"),
+            firstPublishYear = null,
+            ratingCount = 5,
+            numPages = 100,
+            numEditions = 3
+        )
+    }
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {}
+    )
 }
